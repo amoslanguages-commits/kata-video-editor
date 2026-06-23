c4811ad798c32e9051041feffd67aa9cc9a5bb33
amoslanguages-commits <amoslanguages@gmail.com>
2026-06-23 16:44:55 +0200

    Add advanced export settings tab

--- lib/presentation/widgets/export/export_hub_panel.dart
+++ lib/presentation/widgets/export/export_hub_panel.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:nle_editor/core/theme/app_theme.dart';
+import 'package:nle_editor/presentation/widgets/export/export_advanced_settings_card.dart';
 import 'package:nle_editor/presentation/widgets/export/export_pipeline_panel.dart';
 import 'package:nle_editor/presentation/widgets/export/export_preset_builder_panel.dart';
 import 'package:nle_editor/presentation/widgets/export/export_queue_cleanup_panel.dart' as history;
@@ -16,7 +17,7 @@ class ExportHubPanel extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 3,
+      length: 4,
       child: Column(
         children: [
           Container(
@@ -25,6 +26,7 @@ class ExportHubPanel extends StatelessWidget {
               tabs: [
                 Tab(icon: Icon(Icons.history_rounded), text: 'Queue'),
                 Tab(icon: Icon(Icons.tune_rounded), text: 'Builder'),
+                Tab(icon: Icon(Icons.settings_suggest_rounded), text: 'Advanced'),
                 Tab(icon: Icon(Icons.more_horiz_rounded), text: 'More'),
               ],
             ),
@@ -34,6 +36,12 @@ class ExportHubPanel extends StatelessWidget {
               children: [
                 ExportPipelinePanel(projectId: projectId),
                 ExportPresetBuilderPanel(projectId: projectId),
+                ListView(
+                  padding: const EdgeInsets.all(16),
+                  children: [
+                    ExportAdvancedSettingsCard(projectId: projectId),
+                  ],
+                ),
                 history.ExportQueueCleanupPanel(projectId: projectId),
               ],
             ),
