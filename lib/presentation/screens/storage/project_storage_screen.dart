224d51f856b269f40a2f87e405a63aa9dacaf8ff
amoslanguages-commits <amoslanguages@gmail.com>
2026-06-23 13:57:14 +0200

    Use export hub in project storage screen

--- lib/presentation/screens/storage/project_storage_screen.dart
+++ lib/presentation/screens/storage/project_storage_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:nle_editor/core/theme/app_theme.dart';
 import 'package:nle_editor/presentation/widgets/device/device_capability_card.dart';
-import 'package:nle_editor/presentation/widgets/export/export_pipeline_panel.dart';
+import 'package:nle_editor/presentation/widgets/export/export_hub_panel.dart';
 import 'package:nle_editor/presentation/widgets/panels/project_media_management_panel.dart';
 import 'package:nle_editor/presentation/widgets/storage/project_storage_panel.dart';
 
@@ -44,7 +44,7 @@ class ProjectStorageScreen extends StatelessWidget {
               ],
             ),
             ProjectMediaManagementPanel(projectId: projectId),
-            ExportPipelinePanel(projectId: projectId),
+            ExportHubPanel(projectId: projectId),
           ],
         ),
       ),
